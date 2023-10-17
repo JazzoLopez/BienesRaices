60f17a7a1faf22ac2db4ec474ea58f82bc255434
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-10-16 23:12:12 -0600

    Creando servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola mundo, estoy cambiandolo mientras nodemon inicia, ejecuta y finaliza!!"); //* Prueba para ejecutar el script en la terminal 
+const express = require('express')  //*Extraer la dependencia que hemos descargado
+
+//*Crear la app, esta contiene toda la info de express
+const app = express ();
+
+//* Definir un puerto y arrancar nuestro proyecto
+//* Soporta metodos HTTP 
+//* Routing */
+app.get('/', (req, res) =>{
+    res.json({msg:"Hola mundo en express"});
+})
+
+
+const port = 3000;
+
+app.listen(port, () =>{
+    console.log(`El servidor esta funcionando en el puerto: ${port}`);
+})
